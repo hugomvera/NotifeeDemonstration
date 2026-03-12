b170a3daeaa4609aa4f097bb01fb175da98267c8
hugo <HUGOMVERA@GMAIL.COM>
2026-03-11 21:06:03 -0700

    the update notifcation has worked

--- App.js
+++ App.js
@@ -17,8 +17,36 @@ export default function Screen() {
 
     // Display a notification
     await notifee.displayNotification({
+      id: '123',
       title: 'Notification Title',
       body: 'Main body content of the notification',
+      android: {
+        channelId,
+        smallIcon: 'ic_launcher', // optional, defaults to 'ic_launcher'.
+        // pressAction is needed if you want the notification to open the app when pressed
+        pressAction: {
+          id: 'default',
+        },
+      },
+    });
+  }
+
+  //will place the notifcation funciton here
+  async function updateNotification() {
+    // Request permissions (required for iOS)
+    //   await notifee.requestPermission();
+
+    // Create a channel (required for Android)
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    // Display a notification
+    await notifee.displayNotification({
+      id: '123',
+      title: 'Notification Title2',
+      body: 'the notification has changed',
       android: {
         channelId,
         smallIcon: 'ic_launcher', // optional, defaults to 'ic_launcher'.
@@ -37,6 +65,11 @@ export default function Screen() {
           title="Display Notification"
           onPress={() => onDisplayNotification()}
         />
+        //will add a second button to update the notification
+        <Button
+          title="Update Notifcation"
+          onPress={() => updateNotification()}
+        />
       </SafeAreaView>
     </View>
   );
@@ -45,6 +78,8 @@ export default function Screen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margin: 5,
+    padding: 5,
     backgroundColor: '#fff', // Ensures the safe area matches your app background
   },
   content: {
