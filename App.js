5a8f3cbbb3db43905c8d3e4f326ac5eb090fe071
hugo <HUGOMVERA@GMAIL.COM>
2026-03-11 20:19:04 -0700

    the notfication works will add a button when preesssed will update the notification

--- App.js
+++ App.js
@@ -4,10 +4,39 @@ import notifee from '@notifee/react-native';
 
 //need to put in export default so it renders
 export default function Screen() {
+  //will place the notifcation funciton here
+  async function onDisplayNotification() {
+    // Request permissions (required for iOS)
+    await notifee.requestPermission();
+
+    // Create a channel (required for Android)
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    // Display a notification
+    await notifee.displayNotification({
+      title: 'Notification Title',
+      body: 'Main body content of the notification',
+      android: {
+        channelId,
+        smallIcon: 'ic_launcher', // optional, defaults to 'ic_launcher'.
+        // pressAction is needed if you want the notification to open the app when pressed
+        pressAction: {
+          id: 'default',
+        },
+      },
+    });
+  }
+
   return (
     <View style={styles.content}>
       <SafeAreaView>
-        <Button title="Display Notification" onPress={() => {}} />
+        <Button
+          title="Display Notification"
+          onPress={() => onDisplayNotification()}
+        />
       </SafeAreaView>
     </View>
   );
